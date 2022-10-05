4259f8b72108aa7ef3b0b477e7570ffdd23b30f4
Muhammed <muhammedazeez473@gmail.com>
2022-10-05 14:23:47 +0530

    add bubblesort

--- app.go
+++ app.go
@@ -2,8 +2,24 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println(fact(4))
+func bubbleSort() {
+	var array1 [10]int
+	var num int
+	fmt.Println("enter a integer number")
+	fmt.Scanf("%d", &num)
+	fmt.Printf("enter %d values", num)
+	for i := 0; i < num; i++ {
+		fmt.Scanf("%d", &array1[i])
+	}
+	fmt.Println(array1)
+	for i := 0; i < num-1; i++ {
+		for j := 0; j < num-i-1; j++ {
+			if array1[j] < array1[j+1] {
+				array1[j], array1[j+1] = array1[j+1], array1[j]
+			}
+		}
+	}
+	fmt.Println(array1)
 }
 func fact(num int) int {
 	if num == 0 || num == 1 {
