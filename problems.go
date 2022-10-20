3f873da7d0d90ecd03a8f99c03d5d44a81ce9bcb
Muhammed <muhammedazeez473@gmail.com>
2022-10-20 11:44:46 +0530

    max and min

--- problems.go
+++ problems.go
@@ -2,6 +2,77 @@ package main
 
 import "fmt"
 
+func main2() {
+	var array = []int{1, 2, 4, 6, 2, 8, 9, 7, 6, 3, 2}
+	f := 2
+	var count int
+	for _, v := range array {
+		if f == v {
+			count++
+		}
+	}
+	fmt.Println(count)
+}
+
+func minMax1() {
+	var array = []int{1, 3, 5, 7, 9}
+	var sum int
+	var max int
+	var min int
+	for _, v := range array {
+		sum = sum + v
+	}
+	for i, v := range array {
+		d := sum - v
+		if i == 0 {
+			max = d
+			min = d
+		}
+		if d > max {
+			max = d
+		}
+		if d < min {
+			min = d
+		}
+	}
+	fmt.Println(min, max)
+}
+
+func minMax() {
+	var array = []int{-10, -3, 5, 6, -2, -5, -7, -9}
+	var max int = array[0]
+	var min int = array[0]
+	for _, v := range array {
+		if v < min {
+			min = v
+		}
+		if v > max {
+			max = v
+		}
+	}
+	fmt.Printf("%v,%v\n", min, max)
+}
+func min() {
+	var array = []int{-10, -3, 5, 6, -2, -5, -7, -9}
+	var min int = array[0]
+	for _, v := range array {
+		if v < min {
+			min = v
+		}
+	}
+	fmt.Println(min)
+}
+func max() {
+	var array = []int{-10, -3, 5, 6, -2, -5, -7, -9}
+	var max int = array[0]
+	for _, v := range array {
+		if v > max {
+			max = v
+		}
+	}
+	fmt.Println(max)
+}
+
 func main() {
 	var rows int
 	var columns int
@@ -17,7 +88,7 @@ func main() {
 	}
 	for r := 1; r < rows-1; r++ {
 		for c := 1; c < columns-1; c++ {
-			if matrix[r-1][c] == 1 && matrix[r+1][c] == 1 && matrix[r+1][c] == 1 && matrix[r+1][c] == 1 {
+			if matrix[r-1][c] == 1 && matrix[r+1][c] == 1 && matrix[r][c-1] == 1 && matrix[r][c+1] == 1 {
 				matrix[r][c] = 1
 			}
 		}
